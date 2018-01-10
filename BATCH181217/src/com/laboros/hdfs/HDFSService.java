9b324b8632b1dcd31d6419bcd0a978ce23c55a36
svaduka <sainagaraju.vaduka@gmail.com>
2018-01-10 10:04:54 +0530

    Created HDFSService class

---
+++ BATCH181217/src/com/laboros/hdfs/HDFSService.java
@@ -0,0 +1,15 @@
+package com.laboros.hdfs;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HDFSService extends Configured implements Tool{
+	public static void main(String[] args) {
+	}
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		return 0;
+	}
+
+}
